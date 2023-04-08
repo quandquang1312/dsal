6a3ed118352b4b4110678df723009f4064706731
quandquang1312 <quangquanb96@gmail.com>
2023-04-08 14:06:52 +0700

    add template

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define MAXN 200100
+#define FOR1(a,b) for (int i=a; i<b; i++)
+#define FOR2(a,b) for (int j=a; j<b; j++)
+#define LOG(x) cout << x << "\n";
+
+int t;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
