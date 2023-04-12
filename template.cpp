a425832c8aa3272837c9a0d9a1e72636ba8471f9
quandquang1312 <quangquanb96@gmail.com>
2023-04-12 10:55:15 +0700

    edit template

--- template.cpp
+++ template.cpp
@@ -4,11 +4,11 @@ using namespace std;
 typedef long long ll;
 
 #define MAXN 200100
-#define FOR1(a,b) for (int i=a; i<b; i++)
-#define FOR2(a,b) for (int j=a; j<b; j++)
+#define FOR(i,a,b) for (int i=a; i<b; i++) 
 #define LOG(x) cout << x << "\n";
 
-int t;
+int t, n;
+ll arr[MAXN];
 
 void solve()
 {
@@ -19,7 +19,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
     
-    cin >> t;
+    t = 1;
     while (t--)
         solve();
     return 0;
