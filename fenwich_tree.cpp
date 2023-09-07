771f4765d16e5f79e4420e31e9cd92dd90c165c5
quandquang1312 <quangquanb96@gmail.com>
2023-09-07 21:30:49 +0700

    update fenwick tree:

---
+++ fenwich_tree.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class FenwickTree {
+public:
+    FenwickTree(int n) {
+        this->n = n;
+        ft.assign(n, 0);
+    }
+
+    FenwickTree(vector<int> arr) : FenwickTree(arr.size()) {
+        for (int i=0; i<arr.size(); i++)
+            add(i, arr[i]);
+    }
+
+    int sum(int i) {
+        int rs = 0;
+        while (i >= 0) {
+            rs += ft[i];
+            i = g(i) - 1;
+        }
+        return rs;
+    }
+
+    int query(int l, int r) {
+        return sum(r) - sum(l-1);
+    }
+
+    void add(int idx, int vl) {
+        for (; idx < n; idx = idx | (idx + 1))
+            ft[idx] += vl;
+    }
+
+private:
+    vector<int> ft;
+    int n;
+
+private:
+    int g(int i) { return i & (i + 1); }
+};
+
+int main() {
+    vector<int> arr = {0,1,2,3,4,5,6,7};
+    FenwickTree ft = FenwickTree(arr);
+    for (int i=0; i<arr.size(); i++) {
+        cout << ft.query(0,i) << " ";
+    }
+    cout << endl;
+}
