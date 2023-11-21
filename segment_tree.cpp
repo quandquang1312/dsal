9cb594dc68e7e8f1130eebb9093109dd30b7f4a9
Quang Quan <91424630+quandquang1312@users.noreply.github.com>
2023-11-21 16:33:11 +0700

    Update segment_tree.cpp
    class st

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,24 +1,58 @@
-#include <bits/stdc++.h>
-using namespace std;
+class SegmentTree {
+private:
+    std::vector<int> tree;
+    int n;
 
-#define MAXN 200100
-int arr[MAXN], st[MAXN*4];
+    void build(const std::vector<int>& nums, int node, int start, int end) {
+        if (start == end) {
+            tree[node]= nums[start];
+            return;
+        }
 
-void build(int node, int l, int r) {
-    if (l == r) st[node] = arr[l];
-    else {
-        int mid = (l+r) >> 1;
-        build(node*2, l, mid);
-        build(node*2+1, mid+1, r);
-        // for sum segment tree
-        st[node] = st[node*2] + st[node*2+1];
+        int mid = (start + end) / 2;
+        build(nums, 2 * node, start, mid);
+        build(nums, 2 * node + 1, mid + 1, end);
+        tree[node]= tree[2 * node]+ tree[2 * node + 1];
     }
-}
 
-int query(int node, int l, int r, int tl, int tr) {
-    if (l <= tl && tr <= r) return st[node];
-    if (r < tl || tr < l) return 0; // for sum segment tree
-    int mid = (l+r) >> 1;
-    // for sum segment tree
-    return query(node*2, l, r, tl, mid) + query(node*2+1, l, r, mid+1, tr);
-}
+    void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            tree[node]= val;
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        if (idx <= mid)
+            update(2 * node, start, mid, idx, val);
+        else
+            update(2 * node + 1, mid + 1, end, idx, val);
+        tree[node]= tree[2 * node]+ tree[2 * node + 1];
+    }
+
+    int query(int node, int start, int end, int l, int r) {
+        if (r < start || l > end)
+            return 0;
+        if (l <= start && r >= end)
+            return tree[node];
+
+        int mid = (start + end) / 2;
+        int leftSum = query(2 * node, start, mid, l, r);
+        int rightSum = query(2 * node + 1, mid + 1, end, l, r);
+        return leftSum + rightSum;
+    }
+
+public:
+    SegmentTree(const std::vector<int>& nums) {
+        n = nums.size();
+        tree.resize(4 * n);
+        build(nums, 1, 1, n);
+    }
+
+    void update(int idx, int val) {
+        update(1, 1, n, idx, val);
+    }
+
+    int query(int l, int r) {
+        return query(1, 1, n, l, r);
+    }
+};
